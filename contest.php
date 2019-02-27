af846f7e0bb68b74294ec6b7f3d255d5ef5cd7e4
Robin Delbaere <hello@rdelbaere.fr>
2019-02-27 09:05:33 +0100

    Correction : boucle

--- contest.php
+++ contest.php
@@ -15,8 +15,36 @@ if( !$user || !$user['premium'] ){
     <head>
         <meta charset="utf-8">
         <title>GameList</title>
+
+        <style>
+            .contest{
+                display: flex;
+                flex-wrap: wrap;
+            }
+
+            .contest-box{
+                width: 200px;
+                height: 200px;
+                background-color: blue;
+                margin: 20px;
+                color: white;
+                font-size: 4rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                text-decoration: none;
+            }
+        </style>
     </head>
     <body>
-        Page Concours
+        <h2>Concours</h2>
+
+        <div class="contest">
+            <?php for( $i = 0; $i < 10; $i++ ){ ?>
+                <a href="contest.php" class="contest-box">
+                    <?php echo $i + 1; ?>
+                </a>
+            <?php } ?>
+        </div>
     </body>
 </html>
--- list.php
+++ list.php
@@ -30,9 +30,54 @@ $games = [
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>Liste - GameList</title>
+
+        <style>
+            table img{
+                height: 100px;
+            }
+
+            tr.white{
+                background-color: #ecf0f1;
+            }
+            tr.grey{
+                background-color: #bdc3c7;
+            }
+        </style>
     </head>
     <body>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Affiche</th>
+                    <th>Nom</th>
+                    <th>Type</th>
+                    <th>Lien</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                <?php foreach( $games as $key => $game ){ ?>
+                    <tr class="<?php echo ( $key % 2 ) ? 'white' : 'grey' ?>">
+                        <td>
+                            <img src="<?php echo $game['poster']; ?>" alt="Affiche du jeu">
+                        </td>
+                        <td>
+                            <strong><?php echo $game['name']; ?></strong>
+                        </td>
+                        <td><?php echo $game['type']; ?></td>
+                        <td>
+                            <a href="show.php">Voir plus de détail</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+
+            </tbody>
+        </table>
+
+        <?php  ?>
 
     </body>
 </html>
